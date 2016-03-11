5f7e88572198a8e8b426dbc80c6cb7a785e1b9d6
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2016-03-11 11:57:13 +0000

    Moments --> DescStat (gains min/max); CalcMean, CalcVar, CalcSD

--- AlphaStatMod.f90
+++ AlphaStatMod.f90
@@ -10,29 +10,46 @@ module AlphaStatMod
   implicit none
 
   private
-  public :: CalcMoments,MomentsS,MomentsD
+  public :: CalcMean,CalcVar,CalcSD
+  public :: CalcDescStat,DescStatS,DescStatD
   public :: CalcCorrelation,CorrelationS,CorrelationD
 
-  type :: MomentsS
+  interface CalcMean
+    module procedure CalcMeanS, CalcMeanD
+  end interface
+
+  interface CalcVar
+    module procedure CalcVarS, CalcVarD
+  end interface
+
+  interface CalcSD
+    module procedure CalcSDS, CalcSDD
+  end interface
+
+  type :: DescStatS
     integer(int32) :: n
     real(real32)   :: Mean
     real(real32)   :: Var
     real(real32)   :: SD
     real(real32)   :: Skew ! TODO: do we ever need skewness?
     real(real32)   :: Curt ! TODO: do we ever need curtosis?
+    real(real32)   :: Min
+    real(real32)   :: Max
   end type
 
-  type :: MomentsD
+  type :: DescStatD
     integer(int32) :: n
     real(real64)   :: Mean
     real(real64)   :: Var
     real(real64)   :: SD
     real(real64)   :: Skew
     real(real64)   :: Curt
+    real(real64)   :: Min
+    real(real64)   :: Max
   end type
 
-  interface CalcMoments
-    module procedure CalcMomentsS, CalcMomentsD
+  interface CalcDescStat
+    module procedure CalcDescStatS, CalcDescStatD
   end interface
 
   type :: CorrelationS
@@ -54,6 +71,237 @@ module AlphaStatMod
   end interface
 
   contains
+
+    !###########################################################################
+
+    subroutine CalcMeanS(x,Out)
+      implicit none
+
+      ! Arguments
+      real(real32),intent(in)  :: x(:)
+      real(real32),intent(out) :: Out
+
+      Out=sum(x(:))/real(size(x))
+    end subroutine
+
+    !###########################################################################
+
+    subroutine CalcMeanD(x,Out)
+      implicit none
+
+      ! Arguments
+      real(real64),intent(in)  :: x(:)
+      real(real64),intent(out) :: Out
+
+      Out=sum(x(:))/dble(size(x))
+    end subroutine
+
+    !###########################################################################
+
+    subroutine CalcVarS(x,Out,Mean)
+      implicit none
+
+      ! Arguments
+      real(real32),intent(in)  :: x(:)
+      real(real32),intent(out) :: Out
+      real(real32),intent(in),optional :: Mean
+
+      ! Other
+      integer(int32) i,n
+
+      real(real32) :: nR,MeanIn,Dev
+
+      n=size(x)
+      if (n < 1) then
+        write(STDERR,"(a)") "ERROR: number of records must be at least 2 for CalcVar"
+        write(STDERR,"(a)") " "
+        stop 1
+      end if
+
+      nR=real(n)
+      if (.not.present(Mean)) then
+        MeanIn=sum(x(:))/nR
+      else
+        MeanIn=Mean
+      end if
+      Out=0.0
+
+      do i=1,n
+        Dev=x(i)-MeanIn
+        Out=Out+Dev*Dev
+      end do
+
+      Out=Out/(nR-1.0)
+    end subroutine
+
+    !###########################################################################
+
+    subroutine CalcVarD(x,Out,Mean)
+      implicit none
+
+      ! Arguments
+      real(real64),intent(in)  :: x(:)
+      real(real64),intent(out) :: Out
+      real(real64),intent(in),optional :: Mean
+
+      ! Other
+      integer(int32) i,n
+
+      real(real64) :: nR,MeanIn,Dev
+
+      n=size(x)
+      if (n < 1) then
+        write(STDERR,"(a)") "ERROR: number of records must be at least 2 for CalcVar"
+        write(STDERR,"(a)") " "
+        stop 1
+      end if
+
+      nR=dble(n)
+      if (.not.present(Mean)) then
+        MeanIn=sum(x(:))/nR
+      else
+        MeanIn=Mean
+      end if
+      Out=0.0d0
+
+      do i=1,n
+        Dev=x(i)-MeanIn
+        Out=Out+Dev*Dev
+      end do
+
+      Out=Out/(nR-1.0d0)
+    end subroutine
+
+    !###########################################################################
+
+    subroutine CalcSDS(x,Out,Mean)
+      implicit none
+
+      ! Arguments
+      real(real32),intent(in)  :: x(:)
+      real(real32),intent(out) :: Out
+      real(real32),intent(in),optional :: Mean
+
+      if (present(Mean)) then
+        call CalcVarS(x,Out,Mean)
+      else
+        call CalcVarS(x,Out)
+      end if
+      Out=sqrt(Out)
+    end subroutine
+
+    !###########################################################################
+
+    subroutine CalcSDD(x,Out,Mean)
+      implicit none
+
+      ! Arguments
+      real(real64),intent(in)  :: x(:)
+      real(real64),intent(out) :: Out
+      real(real64),intent(in),optional :: Mean
+
+      if (present(Mean)) then
+        call CalcVarD(x,Out,Mean)
+      else
+        call CalcVarD(x,Out)
+      end if
+      Out=sqrt(Out)
+    end subroutine
+
+    !###########################################################################
+
+    subroutine CalcDescStatS(x,Out)
+      implicit none
+
+      ! Arguments
+      real(real32),intent(in)   :: x(:)
+      type(DescStatS),intent(out) :: Out
+
+      ! Other
+      integer(int32) i,n
+      real(real32) :: nR,Dev,Mul
+
+      n=size(x)
+      if (n < 1) then
+        write(STDERR,"(a)") "ERROR: number of records must be at least 2 for CalcDescStat"
+        write(STDERR,"(a)") " "
+        stop 1
+      end if
+
+      nR=real(n)
+      Out%Mean=sum(x(:))/nR
+      Out%Var=0.0
+      Out%Skew=0.0
+      Out%Curt=0.0
+
+      do i=1,n
+        Dev=x(i)-Out%Mean
+        Mul=Dev*Dev
+        Out%Var=Out%Var+Mul
+        Mul=Mul*Dev
+        Out%Skew=Out%Skew+Mul
+        Mul=Mul*Dev
+        Out%Curt=Out%Curt+Mul
+      end do
+
+      Out%Var=Out%Var/(nR-1.0)
+      Out%SD=sqrt(Out%Var)
+      if (Out%Var > 0.0) then
+        Out%Skew=Out%Skew/(nR*Out%SD**3)
+        Out%Curt=Out%Curt/(nR*Out%Var**2)-3.0
+      end if
+
+      Out%Min=minval(x(:))
+      Out%Max=maxval(x(:))
+    end subroutine
+
+    !###########################################################################
+
+    subroutine CalcDescStatD(x,Out)
+      implicit none
+
+      ! Arguments
+      real(real64),intent(in)   :: x(:)
+      type(DescStatD),intent(out) :: Out
+
+      ! Other
+      integer(int32) i,n
+
+      real(real64) :: nR,Dev,Mul
+
+      n=size(x)
+      if (n < 1) then
+        write(STDERR,"(a)") "ERROR: number of records must be at least 2 for CalcDescStat"
+        write(STDERR,"(a)") " "
+        stop 1
+      end if
+
+      nR=dble(n)
+      Out%Mean=sum(x(:))/nR
+      Out%Var=0.0d0
+      Out%Skew=0.0d0
+      Out%Curt=0.0d0
+
+      do i=1,n
+        Dev=x(i)-Out%Mean
+        Mul=Dev*Dev
+        Out%Var=Out%Var+Mul
+        Mul=Mul*Dev
+        Out%Skew=Out%Skew+Mul
+        Mul=Mul*Dev
+        Out%Curt=Out%Curt+Mul
+      end do
+
+      Out%Var=Out%Var/(nR-1.0d0)
+      Out%SD=sqrt(Out%Var)
+      if (Out%Var > 0.0d0) then
+        Out%Skew=Out%Skew/(nR*Out%SD**3)
+        Out%Curt=Out%Curt/(nR*Out%Var**2)-3.0d0
+      end if
+
+      Out%Min=minval(x(:))
+      Out%Max=maxval(x(:))
+    end subroutine
 
     !###########################################################################
 
@@ -119,95 +367,6 @@ module AlphaStatMod
       Out%Cov=SumXY/(nR-1.0d0)
       Out%Var1=SumXX/(nR-1.0d0)
       Out%Var2=SumYY/(nR-1.0d0)
-    end subroutine
-
-    !###########################################################################
-
-    subroutine CalcMomentsS(x,Out)
-      implicit none
-
-      ! Arguments
-      real(real32),intent(in)   :: x(:)
-      type(MomentsS),intent(out) :: Out
-
-      ! Other
-      integer(int32) i,n
-      real(real32) :: nR,Dev,Mul
-
-      n=size(x)
-      if (n < 1) then
-        write(STDERR,"(a)") "ERROR: number of records must be at least 2 for CalcMoments"
-        write(STDERR,"(a)") " "
-        stop 1
-      end if
-
-      nR=real(n)
-      Out%Mean=sum(x(:))/nR
-      Out%Var=0.0
-      Out%Skew=0.0
-      Out%Curt=0.0
-
-      do i=1,n
-        Dev=x(i)-Out%Mean
-        Mul=Dev*Dev
-        Out%Var=Out%Var+Mul
-        Mul=Mul*Dev
-        Out%Skew=Out%Skew+Mul
-        Mul=Mul*Dev
-        Out%Curt=Out%Curt+Mul
-      end do
-
-      Out%Var=Out%Var/(nR-1.0)
-      Out%SD=sqrt(Out%Var)
-      if (Out%Var > 0.0) then
-        Out%Skew=Out%Skew/(nR*Out%SD**3)
-        Out%Curt=Out%Curt/(nR*Out%Var**2)-3.0
-      end if
-    end subroutine
-
-    !###########################################################################
-
-    subroutine CalcMomentsD(x,Out)
-      implicit none
-
-      ! Arguments
-      real(real64),intent(in)   :: x(:)
-      type(MomentsD),intent(out) :: Out
-
-      ! Other
-      integer(int32) i,n
-
-      real(real64) :: nR,Dev,Mul
-
-      n=size(x)
-      if (n < 1) then
-        write(STDERR,"(a)") "ERROR: number of records must be at least 2 for CalcMoments"
-        write(STDERR,"(a)") " "
-        stop 1
-      end if
-
-      nR=dble(n)
-      Out%Mean=sum(x(:))/nR
-      Out%Var=0.0d0
-      Out%Skew=0.0d0
-      Out%Curt=0.0d0
-
-      do i=1,n
-        Dev=x(i)-Out%Mean
-        Mul=Dev*Dev
-        Out%Var=Out%Var+Mul
-        Mul=Mul*Dev
-        Out%Skew=Out%Skew+Mul
-        Mul=Mul*Dev
-        Out%Curt=Out%Curt+Mul
-      end do
-
-      Out%Var=Out%Var/(nR-1.0d0)
-      Out%SD=sqrt(Out%Var)
-      if (Out%Var > 0.0d0) then
-        Out%Skew=Out%Skew/(nR*Out%SD**3)
-        Out%Curt=Out%Curt/(nR*Out%Var**2)-3.0d0
-      end if
     end subroutine
 
     !###########################################################################
