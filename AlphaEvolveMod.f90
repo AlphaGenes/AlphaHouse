184286cde6fe0dd64772f15844fc83e4cd02b0ca
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2016-03-26 20:39:04 -0400

    Revert "Revert "Making sure we use all provided initial solutions""
    This reverts commit 79e38029e1e70899df8ff33562da6ead80714f01.

--- AlphaEvolveMod.f90
+++ AlphaEvolveMod.f90
@@ -147,6 +147,7 @@ module AlphaEvolveMod
           OldChrom(:,Sol)=Init(:,Sol)
           call CalcCriterion(OldChrom(:,Sol),CritType,Criterion(Sol))
         end do
+        nInit=Sol
       else
         nInit=1
       end if
