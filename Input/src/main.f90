ca1c9cfd02906978ec7b99bb174bc16f380a3890
Diarmaid de Burca <diarmaiddeburcaroslin@gmail.com>
2016-10-16 22:39:52 +0100

    Changed from function to subroutine

--- Input/src/main.f90
+++ Input/src/main.f90
@@ -4,17 +4,9 @@ implicit none
 
  character(len=:), allocatable:: fileName
 
- fileName = getInputFile()
+ call getInputFile(filename)
 
  write(*,*) fileName, len(fileName)
-
-!read(*, *) file
-!if (trim(file) =="d") then
-!Call initialise()
-!else
-!call initialise(file)
-!end if
-!write(*,*) inputFileName, len(inputFileName)
 
 end Program
 
