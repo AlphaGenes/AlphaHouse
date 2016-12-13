1df417dbd769d5d5fd32b1f05b5da44bb5d6a0f7
WILSON David <david.wilson@roslin.ed.ac.uk>
2016-12-13 13:55:13 +0000

    moved files into more managable folders

--- src/ConstantModule.f90
+++
@@ -1,9 +0,0 @@
-module ConstantModule
-
-    integer, parameter :: MissingPhaseCode = 9
-    integer, parameter :: ErrorPhaseCode = -1
-    integer, parameter :: MissingGenotypeCode = 3
-
-
-
-end Module ConstantModule
