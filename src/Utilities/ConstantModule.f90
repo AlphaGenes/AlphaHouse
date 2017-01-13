df8b1c0e6dd07b9f55f7f9b3802760047df91a44
MONEY Daniel <Daniel.Money@roslin.ed.ac.uk>
2017-01-13 15:20:35 +0000

    Add constant

--- src/Utilities/ConstantModule.f90
+++ src/Utilities/ConstantModule.f90
@@ -3,6 +3,7 @@ module ConstantModule
     integer, parameter :: MissingPhaseCode = 9
     integer, parameter :: ErrorPhaseCode = -1
     integer, parameter :: MissingGenotypeCode = 3
+    integer, parameter :: MissingHaplotypeCode = -99
     character, parameter :: EMPTY_PARENT = '0'
     integer, parameter :: IDLENGTH = 32
     integer, parameter :: generationThreshold = 1000
