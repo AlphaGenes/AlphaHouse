0bea08c6f896043a8c60290aa2256a8946c5e0d5
David Wilson <david.wilson@roslin.ed.ac.uk>
2017-05-03 10:18:17 +0100

    removed debug statement

--- src/Utilities/IntegerLinkedListModule.f90
+++ src/Utilities/IntegerLinkedListModule.f90
@@ -235,7 +235,6 @@ contains
                 this%length = this%length - 1 
                 if (.not. associated(this%first)) then
                     deallocate(this%last)
-                    print *, "LIST EMPTY:", this%length
                 else 
                     this%first%previous => null()                    
                 endif
