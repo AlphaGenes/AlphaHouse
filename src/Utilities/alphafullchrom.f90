8f79d66cafe0659c0de964b44e1e60de6c57d0aa
David Wilson <david.wilson@roslin.ed.ac.uk>
2017-08-07 17:14:00 +0100

    added new full chrom thoughts

--- src/Utilities/alphafullchrom.f90
+++ src/Utilities/alphafullchrom.f90
@@ -11,13 +11,23 @@ contains
 
 
 
+        ! TODO currently only opperate on a single pedigree, can be chnaged to take in more using array
+        ! Will use more memory
+        ped = PedigreeHolder( )
+
         do i=1, nChroms
 
             write(chromPath,'(a,i0)') "chr",i
             result=makedirqq(prepend//trim(chromPath))
-
             if (result /= 0) then
                 write(error_unit,*) "ERROR - creating directories has failed with error code:", result
+            endif
+
+            call ped%wipeGenotypeAndPhaseInfo
+
+
+            call ped%addGenotypeInformationFromFile("genoFile")
+            
         enddo 
 
         
