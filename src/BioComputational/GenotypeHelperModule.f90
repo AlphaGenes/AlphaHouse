91dcb9dda0be7ec216ca5c9a469634455b834479
David Wilson <david.wilson@roslin.ed.ac.uk>
2017-08-25 14:37:49 +0100

    added functions

---
+++ src/BioComputational/GenotypeHelperModule.f90
@@ -0,0 +1,24 @@
+module GenotypeHelperModule
+
+
+
+contains
+
+
+	integer function GiveOppositeGenotype(geno)
+
+		if (Geno == 2) then
+			GiveOppositeGenotype = 0
+		else if (Geno == 0) then
+			GiveOppositeGenotype = 2
+
+		else
+			write(error_unit,*) "ERROR: GiveOppositeGenotype given incorrect value"
+			abort()
+		endif
+
+	end function GiveOppositeGenotype
+
+
+
+end module GenotypeHelperModule 
