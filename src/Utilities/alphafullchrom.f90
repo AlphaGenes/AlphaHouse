947587a8feabb8e62fbd8cbdaf6d7db015bb47a3
David Wilson <david.wilson@roslin.ed.ac.uk>
2017-09-06 17:02:16 +0100

    getting there with plink handling

--- src/Utilities/alphafullchrom.f90
+++ src/Utilities/alphafullchrom.f90
@@ -1,67 +1,66 @@
 module alphaFullChromModule
 
-    use AlphaImputeModule
+	use AlphaImputeModule
 
 
-      interface
-     function runProgram (specFileInput, ped)
-        type(specFilebase) :: specFileInput
-        type(pedigreeHolder) :: ped
-     end function runProgram
+	function runProgram (specFileInput, ped)
+		type(specFilebase) :: specFileInput
+		type(pedigreeHolder) :: ped
+	end function runProgram
 
-contains 
-
-
-    subroutine fullChromSplit(pedigreeFile, specfile, funPointer)
-        implicit none
-        character(len=*) :: pedigreeFile
-        integer :: nChroms, result
-
-        character(len=300) :: chromPath
-
-        procedure(runProgram), pointer, intent(in):: funPointer
-
-        ! TODO currently only opperate on a single pedigree, can be chnaged to take in more using array
-        ! Will use more memory
-        ! This means this can be done in parallel, even mpi
-        ped = PedigreeHolder(pedigreeFile)
-
-        do i=1, nChroms
-
-            specFile%resultFolderPath = chromPath
-            write(chromPath,'(a,i0)') "chr",i
-            result=makedirqq(prepend//trim(chromPath))
-            if (result /= 0) then
-                write(error_unit,*) "ERROR - creating directories has failed with error code:", result
-            endif
-
-            call ped%wipeGenotypeAndPhaseInfo
+	contains
 
 
-            call ped%addGenotypeInformationFromFile("genoFile")
+		subroutine fullChromSplit(pedigreeFile, specfile, funPointer)
+			implicit none
+			character(len=*) :: pedigreeFile
+			integer :: nChroms, result
+
+			character(len=300) :: chromPath
+
+			procedure(runProgram), pointer, intent(in):: funPointer
+
+			! TODO currently only opperate on a single pedigree, can be chnaged to take in more using array
+			! Will use more memory
+			! This means this can be done in parallel, even mpi
+			ped = PedigreeHolder(pedigreeFile)
+
+			do i=1, nChroms
+
+				specFile%resultFolderPath = chromPath
+				write(chromPath,'(a,i0)') "chr",i
+				result=makedirqq(prepend//trim(chromPath))
+				if (result /= 0) then
+					write(error_unit,*) "ERROR - creating directories has failed with error code:", result
+				endif
+
+				call ped%wipeGenotypeAndPhaseInfo
 
 
-            call funPointer(specFile,ped)
-            ! cwd for program should be settable
-            ! Can be done using CHDIR command
-            
-        enddo 
+				call ped%addGenotypeInformationFromFile("genoFile")
 
-        
 
-    end subroutine fullChromSplit
+				call funPointer(specFile,ped)
+				! cwd for program should be settable
+				! Can be done using CHDIR command
+
+			enddo
 
 
 
-
-subroutine parseMapFile(filename)
-
-    character(len=*) :: filename
-
-    
+		end subroutine fullChromSplit
 
 
-end subroutine parseMapFile
+
+
+		subroutine parseMapFile(filename)
+
+			character(len=*) :: filename
+
+
+
+
+		end subroutine parseMapFile
 
 
 end module alphaFullChromModule
