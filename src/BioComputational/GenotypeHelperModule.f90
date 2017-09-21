74387b9caa4e3d28b065f076bbea504ed4b34151
GONEN Serap <Serap.Gonen@roslin.ed.ac.uk>
2017-09-21 17:01:25 +0100

    added isopposing

--- src/BioComputational/GenotypeHelperModule.f90
+++ src/BioComputational/GenotypeHelperModule.f90
@@ -1,51 +1,55 @@
 !###########################################################################################################################################################
 
 module GenotypeHelperModule
-	use iso_fortran_env
-	implicit none
 
     !###########################################################################################################################################################
 
-	contains
+    contains
+
+        !###########################################################################################################################################################
+        integer function GiveOppositeGenotype(Geno)
+            use iso_fortran_env
+            implicit none
+
+            integer, intent(in) :: Geno
+
+            if (Geno == 2) then
+                GiveOppositeGenotype = 0
+            else if (Geno == 0) then
+                GiveOppositeGenotype = 2
+
+            else
+                write(error_unit,*) "ERROR: GiveOppositeGenotype given incorrect value"
+                call abort()
+            endif
+
+        end function GiveOppositeGenotype
+
+        !###########################################################################################################################################################
+        function IsOpposing(Genotype1, Genotype2)
+            use iso_fortran_env
+            use ConstantModule
+
+            implicit none
+
+            integer, intent(in) :: Genotype1, Genotype2
+            logical :: IsOpposing
+
+            IsOpposing = .false. ! assuming you have already checked it is not het
+
+            if ((Genotype1 == MissingGenotypeCode) .or. (Genotype2 == MissingGenotypeCode)) return 
+
+            if (Genotype1 == Genotype2) then
+                IsOpposing = .false.
+            else
+                IsOpposing = .false. 
+            endif
+
+        end function IsOpposing
 
         !###########################################################################################################################################################
 
-		integer function GiveOppositeGenotype(geno)
 
-			if (Geno == 2) then
-				GiveOppositeGenotype = 0
-			else if (Geno == 0) then
-				GiveOppositeGenotype = 2
-
-			else
-				write(error_unit,*) "ERROR: GiveOppositeGenotype given incorrect value"
-				call abort()
-			endif
-
-		end function GiveOppositeGenotype
-        !###########################################################################################################################################################
-
-	    function IsOpposing(Genotype1, Genotype2)
-	        use ConstantModule
-
-	        implicit none
-
-	        integer, intent(in) :: Genotype1, Genotype2
-	        logical :: IsOpposing
-
-	        IsOpposing = .false. ! assuming you have already checked it is not het
-
-	        if ((Genotype1 == MissingGenotypeCode) .or. (Genotype2 == MissingGenotypeCode)) return 
-
-	        if (Genotype1 == Genotype2) then
-	            IsOpposing = .false.
-	        else
-	            IsOpposing = .false. 
-	        endif
-
-	    end function IsOpposing
-        !###########################################################################################################################################################
     !###########################################################################################################################################################
-
 end module GenotypeHelperModule 
 !###########################################################################################################################################################
