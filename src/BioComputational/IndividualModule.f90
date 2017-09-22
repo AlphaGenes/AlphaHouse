c2d158c0bee4c7fef8338054df05e6fe490cb3f2
GONEN Serap <Serap.Gonen@roslin.ed.ac.uk>
2017-09-22 10:15:40 +0100

    plant additions to alphahouse

--- src/BioComputational/IndividualModule.f90
+++ src/BioComputational/IndividualModule.f90
@@ -143,6 +143,7 @@ module IndividualModule
             procedure :: countHighDensityOffspring
             procedure :: addFamily
             procedure :: initialisePlantArrays
+            procedure :: getProbabilitiesFromOwnGenotypeAndPhase
             generic:: write(formatted)=> writeIndividual
 
             
